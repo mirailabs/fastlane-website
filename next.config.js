6d841d694dcc15ff360b8c1dfa11682b8724245a
Preetam D'Souza <preetamjdsouza@gmail.com>
2024-03-22 16:13:40 -0400

    fix: static export errors

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
-// static website export config: paths -> pages
-// see https://zeit.co/blog/next3-preview for more info
-exports.exportPathMap = () => ({
-    "/": { page: "/" },
-    // ...add other pages if needed
-})
+const nextConfig = {
+  output: "export",
+
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+};
+
+module.exports = nextConfig;
